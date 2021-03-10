647b72cc6ada9f75639d3db3e04a826c417b3966
Emma Craig <emma.ryal@gmail.com>
2021-03-10 16:25:51 +0100

    adding options to list

--- monkey_sort.js
+++ monkey_sort.js
@@ -2,7 +2,7 @@
 function ComparisonMatrix(items) {
   var self = this;
   self.items = items;
-  console.log("items-----", items)
+  console.log("items-----", items);
   self.matrix = {};
   self.explicitCount = 0;
 
@@ -84,19 +84,85 @@ function quickSort(items, matrix) {
 
 $(function () {
   var matrix;
-  var items;
+  // var items;
+  var lines = [];
+  var items = [];
 
+  $("#discard-button").click(function () {
+    $("#input").hide();
+    $("#discard-page").show();
+    $("#ask").hide();
+    var lines = [];
+    //show each item in list
+    $.each($("#items").val().split(/\n/), function (i, line) {
+      if (line) {
+        lines.push(line);
+      } else {
+        lines.push("");
+      }
+    });
+    console.log("items from input:", lines);
+
+    // localStorage.setItem("storedLines", JSON.stringify($("#items").val()));
+    localStorage.setItem("lines", lines);
+    console.log("lines length=", lines.length);
+    
+
+    $("#to-be-discarded").html(lines[0]);
+  });
+ 
+  let counter = 0;
+  function nextQuestion() {
+    lines = localStorage.getItem("lines").split(",");
+    console.log("LINES--", localStorage.getItem("lines"));
+    console.log("LINES from variable via localstorage--", lines);
+    // let linesArray = lines.split(",")
+    console.log("lines:", lines);
+    console.log("counter:", counter)
+    counter++;
+    $("#to-be-discarded").html(lines[counter]);
+    if (counter === lines.length) {
+      //7 elements in the array
+      $("#keep-in-list-button").hide();
+      $("#discard-from-list-button").hide();
+      $("#show-trimmed").text(items);
+      $("#submit").show();
+    }
+  }
+
+  // $("#to-be-discarded").html(lines[0]);
+
+  $("#discard-from-list-button").click(() => {
+    nextQuestion();
+    // lines.splice(0, 1);
+    console.log("discarded:", lines[counter - 1]);
+    // $("#to-be-discarded").html(lines[0]);
+  });
+
+  $("#keep-in-list-button").click(() => {
+    nextQuestion();
+    // console.log("lines", lines);
+    // const added = lines.splice(0,1).slice(0,1)
+    // items.push(...added);
+    // $("#to-be-discarded").html(lines[1]);
+    console.log("kept", lines[counter - 1]);
+    items.push(lines[counter - 1]);
+    console.log("items aftertrimming", items);
+  });
+
+  /////////////////////////////////////////////
   $("#submit").click(function (e) {
     e.preventDefault();
-    items = _(
-      _(
-        _($("#items").val().split("\n")).map(function (s) {
-          return s.trim();
-        })
-      ).reject(function (s) {
-        return s === "";
-      })
-    ).uniq();
+    // items = _(
+    //   _(
+    //     _($("#items").val().split("\n")).map(function (s) {
+    //       return s.trim();
+    //     })
+    //   ).reject(function (s) {
+    //     return s === "";
+    //   })
+    // ).uniq();
+    console.log("items after submitting trim", items);
     matrix = new ComparisonMatrix(items);
     tryQuickSort();
   });
@@ -112,6 +178,7 @@ $(function () {
 
   function askUser(a, b) {
     $("#input").hide();
+    $("#discard-page").hide();
     $("#ask").show();
     $("#ask_a").text(a);
     $("#ask_b").text(b);
@@ -131,20 +198,44 @@ $(function () {
     $("#ask").hide();
     $("#results").show();
     $("#results_list").html();
-   
+
     $("#explicit_count").text(matrix.explicitCount);
     $("#explicit_count").show();
     _(items).each(function (item) {
-      $("<li />").appendTo($("#results_list")).text(item);
-      // console.log("explicit count", matrix.explicitCount);
-      console.log("results list", results_list)
-
+     $("<li />").appendTo($("#results_list")).text(item);
+      console.log("explicit count", matrix.explicitCount);
+      console.log("results list", results_list);
     });
   }
 
-  $("#start_over").click(function (e) {
-    e.preventDefault();
+  $("#start_over_clean").click(function (e) {
+    // window.location.replace("list.html");
+    $("#discard-page").hide();
     $("#results").hide();
     $("#input").show();
+    //   lines=[];
+    //   items=[];
+    //   console.log("items at start over:", items);
+    //   console.log("lines at start over:", lines)
+  });
+
+  $("#start_over_same_list").click(function (e) {
+    // e.preventDefault(e);
+    // let lines = JSON.parse(localStorage.getItem("lines"));
+    //window.location.replace("list.html")
+
+    let lines = localStorage.getItem("lines");
+    console.log("lines from local storage", lines);
+    // window.location.replace("list.html")
+    //  lines = lines.join('/n').split()
+    //  console.log("lines from local storage", lines)
+    //   $("#results").hide();
+    $("#input").show();
+    //   lines=[];
+    //   items=[];
+    //   console.log("items at start over:", items);
+    //   console.log("lines at start over:", lines)
+
+    // $("#items").val(lines).show();
   });
 });
